3d6fff67a882d8396fcfc88459bb792f9470f57b
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-07-31 16:36:26 -0400

    use generic mode

--- edgeql.el
+++ edgeql.el
@@ -1,69 +1,47 @@
-(defvar edgeql-constants
-  '("AGGREGATE"
-    "ALTER"
-    "AND"
-    "ANY"
-    "COMMIT"
-    "CREATE"
-    "DELETE"
-    "DETACHED"
-    "DISTINCT"
-    "DROP"
-    "ELSE"
-    "EMPTY"
-    "EXISTS"
-    "FALSE"
-    "FILTER"
-    "FUNCTION"
-    "GET"
-    "GROUP"
-    "IF"
-    "ILIKE"
-    "IN"
-    "INSERT"
-    "IS"
-    "LIKE"
-    "LIMIT"
-    "MODULE"
-    "NOT"
-    "OFFSET"
-    "OR"
-    "ORDER"
-    "OVER"
-    "PARTITION"
-    "ROLLBACK"
-    "SELECT"
-    "SET"
-    "SINGLETON"
-    "START"
-    "TRUE"
-    "UPDATE"
-    "UNION"
-    "WITH"))
-
-(defvar edgeql-keywords
-  '("other-keyword" "another-keyword"))
-
-(defvar edgeql-tab-width 4 "Width of a tab for EDGEQL mode")
-
-(defvar edgeql-font-lock-defaults
-  `((
-     ;; stuff between double quotes
-     ("\"\\.\\*\\?" . font-lock-string-face)
-     ;; ; : , ; { } =>  @ $ = are all special elements
-     (":\\|,\\|;\\|{\\|}\\|=>\\|@\\|$\\|=" . font-lock-keyword-face)
-     ( ,(regexp-opt edgeql-keywords 'words) . font-lock-builtin-face)
-     ( ,(regexp-opt edgeql-constants 'words) . font-lock-constant-face)
-     )))
-
-(define-derived-mode edgeql-mode fundamental-mode "EDGEQL script"
-  "EDGEQL mode is a major mode for editing EDGEQL files"
-  (setq font-lock-defaults edgeql-font-lock-defaults)
-  (when edgeql-tab-width
-    (setq tab-width edgeql-tab-width))
-  (setq comment-start "#")
-  (setq comment-end "")
-  (modify-syntax-entry ?# "< b" edgeql-mode-syntax-table)
-  (modify-syntax-entry ?\n "> b" edgeql-mode-syntax-table))
-
-(provide 'edgeql-mode)
+(define-generic-mode 'edgeql-mode
+  '("#")
+  '("aggregate"
+    "alter"
+    "and"
+    "any"
+    "commit"
+    "create"
+    "delete"
+    "detached"
+    "distinct"
+    "drop"
+    "else"
+    "empty"
+    "exists"
+    "false"
+    "filter"
+    "function"
+    "get"
+    "group"
+    "if"
+    "ilike"
+    "in"
+    "insert"
+    "is"
+    "like"
+    "limit"
+    "module"
+    "not"
+    "offset"
+    "or"
+    "order"
+    "over"
+    "partition"
+    "rollback"
+    "select"
+    "set"
+    "singleton"
+    "start"
+    "true"
+    "update"
+    "union"
+    "with")
+  nil
+  '("\\.edgeql$")
+  nil
+  "EdgeQL mode")
