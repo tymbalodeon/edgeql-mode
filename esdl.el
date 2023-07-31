e14c08237e02b7adab4b5c7e3c166cba02344730
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-07-31 19:30:17 -0400

    update esdl

--- esdl.el
+++ esdl.el
@@ -14,7 +14,11 @@
 (define-generic-mode 'esdl-mode
   '("#")
   (edgeql-add-upcase-keywords esdl-keywords)
-  nil
+  '(("datetime" . 'font-lock-builtin-face)
+    ("str" . 'font-lock-builtin-face)
+    ("bool" . 'font-lock-builtin-face)
+    ("exclusive" . 'font-lock-constant-face)
+    ("int32" . 'font-lock-builtin-face))
   '("\\.esdl$")
   nil
   "EdgeDB Schema Mode")
