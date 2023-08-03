246b82c40c0e4d31ff16cbd2e91ac2123b8b6e31
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-08-03 09:19:19 -0400

    update keywords

--- esdl.el
+++ esdl.el
@@ -4,9 +4,13 @@
   (edgeql-add-upcase-keywords
    (append lowercase-keywords
            '("access"
+             "after"
              "constraint"
              "default"
+             "do"
+             "each"
              "extending"
+             "for"
              "global"
              "link"
              "multi"
@@ -14,22 +18,28 @@
              "property"
              "required"
              "scalar"
+             "trigger"
              "type"))))
 
 (defvar esdl-types
   '("bool"
     "datetime"
     "int32"
-    "str"))
+    "json"
+    "str"
+    "uuid"))
+
+(defvar esdl-builtins '("exclusive"))
 
 (defvar esdl-tab-width 4 "Width of a tab for ESDL mode")
 
 (defvar esdl-font-lock-defaults
-  `(((,(regexp-opt esdl-keywords 'words) . font-lock-keyword-face)
+  `(((,(regexp-opt esdl-builtins 'words) . font-lock-builtin-face)
+     (,(regexp-opt esdl-keywords 'words) . font-lock-keyword-face)
      (,(regexp-opt esdl-types 'words) . font-lock-type-face))))
 
-(define-derived-mode esdl-mode prog-mode "ESDL mode"
-  "ESDL mode is a major mode for editing ESDL files"
+(define-derived-mode esdl-mode prog-mode "EdgeDB Schema"
+  "Major mode for editing EdgeDB Schema definition files"
   (setq font-lock-defaults esdl-font-lock-defaults)
   (when esdl-tab-width
     (setq tab-width esdl-tab-width))
