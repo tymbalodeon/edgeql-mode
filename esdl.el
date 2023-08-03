a0c540795dff9a54e6c016472220e46aabb5eb31
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-08-03 08:59:35 -0400

    use define-derived-mode

--- esdl.el
+++ esdl.el
@@ -1,28 +1,43 @@
 (load! "./helpers.el")
 
 (defvar esdl-keywords
-  (append lowercase-keywords
-          '("access"
-            "constraint"
-            "default"
-            "extending"
-            "global"
-            "link"
-            "multi"
-            "policy"
-            "property"
-            "required"
-            "scalar"
-            "type")))
+  (edgeql-add-upcase-keywords
+   (append lowercase-keywords
+           '("access"
+             "constraint"
+             "default"
+             "extending"
+             "global"
+             "link"
+             "multi"
+             "policy"
+             "property"
+             "required"
+             "scalar"
+             "type"))))
 
-(define-generic-mode 'esdl-mode
-  '("#")
-  (edgeql-add-upcase-keywords esdl-keywords)
-  '(("bool" . 'font-lock-type-face)
-    ("datetime" . 'font-lock-type-face)
-    ("exclusive" . 'font-lock-variable-name-face)
-    ("int32" . 'font-lock-type-face)
-    ("str" . 'font-lock-type-face))
-  '("\\.esdl$")
-  nil
-  "EdgeDB Schema Mode")
+(defvar esdl-types
+  '("bool"
+    "datetime"
+    "int32"
+    "str"))
+
+(defvar esdl-tab-width 4 "Width of a tab for ESDL mode")
+
+(defvar esdl-font-lock-defaults
+  `(((,(regexp-opt esdl-keywords 'words) . font-lock-keyword-face)
+     (,(regexp-opt esdl-types 'words) . font-lock-type-face))))
+
+(define-derived-mode esdl-mode fundamental-mode "ESDL mode"
+  "ESDL mode is a major mode for editing ESDL files"
+  (setq font-lock-defaults esdl-font-lock-defaults)
+  (when esdl-tab-width
+    (setq tab-width esdl-tab-width))
+  (setq comment-start "#")
+  (setq comment-end ""))
+
+(add-to-list 'auto-mode-alist '("\\.esdl$" . esdl-mode))
+
+(add-hook 'esdl-mode-hook 'display-line-numbers-mode)
+
+(provide 'esdl-mode)
